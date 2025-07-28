22ebd6bc490eb055fbacb2b9c89a8d68fefd87ce
diazAman5723P <silasdivine3@gmail.com>
2025-07-28 15:32:35 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " malvin~krlWCSLD#dQAkUkMxLO38685JhtdXPPKBYEc-qdfSQhW2oWFqq64",  // 
-  OWNER_NUMBER: "2347042622369", // PUT OWNER NUMBER HERE
+  SESSION_ID: " malvin~krlWCSLD#dQAkUkMxLO38685JhtdXPPKBYEc-qdfSQhW2oWFqq64",  // malvin~krlWCSLD#dQAkUkMxLO38685JhtdXPPKBYEc-qdfSQhW2oWFqq64
+  OWNER_NUMBER: "2347042622369", // 2347042622369
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
