82c6ff80603cc4ce2d442da183ad14c0d6a18537
diazAman5723P <silasdivine3@gmail.com>
2025-07-28 14:36:36 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: " malvin~hutTFTyR#2ZKQhjZn7Bhtny6LB7orxxG0uWe4DsBhe-G0GTI-LM8",  // malvin~hutTFTyR#2ZKQhjZn7Bhtny6LB7orxxG0uWe4DsBhe-G0GTI-LM8
+  OWNER_NUMBER: "2347042622369", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
